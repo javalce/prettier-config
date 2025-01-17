f4342bd2fca74ddcc3caf62f82022adb8de28326
Javier Valero Cejudo <javalce29@gmail.com>
2025-01-17 11:24:25 +0100

    refactor: migrate bump.config.js to ES module syntax

--- bump.config.js
+++ bump.config.js
@@ -1,6 +1,7 @@
-/** @type {import('bumpp').VersionBumpOptions} */
-module.exports = {
+import { defineConfig } from 'bumpp';
+
+export default defineConfig({
   commit: 'release: v%s',
   tag: 'v%s',
   all: true,
-};
+});
