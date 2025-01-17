38b79611b61e7e0fd52c797798fa6f24a0d6ab28
Javier Valero Cejudo <javalce29@gmail.com>
2025-01-17 12:39:48 +0100

    refactor: move Prettier configuration to index.ts for better organization

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-import { defineConfig } from './config';
+import { type Config } from "prettier";
 
-export default defineConfig();
+export function defineConfig(config?: Config): Config {
+  const { plugins = [], overrides = [], ...customConfig } = config ?? {};
+
+  return {
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    endOfLine: "lf",
+    trailingComma: "all",
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    arrowParens: "always",
+    ...customConfig,
+    plugins: ["prettier-plugin-packagejson", ...plugins],
+    overrides,
+  };
+}
